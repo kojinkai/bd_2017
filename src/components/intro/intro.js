aaa31352923044a75330212a28eeb89ce1a677db
Lewis Nixon <lewcfn@gmail.com>
2017-06-04 16:43:06 +0100

    add an intro component and styles

---
+++ src/components/intro/intro.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './boxdeluxe_logo.svg';
+import './intro.css';
+
+class Intro extends Component {
+  render() {
+    return (
+    <section className="waypoint intro">
+        <div className="content">
+            <div className="intro__brand">
+                <img className="intro__logo" src={logo} alt="box deluxe logo" />
+                <b className="icon-star_icon intro__icon"></b>
+            </div>
+            <p className="intro__text">
+                Hi, I’m Lewis Nixon, a Front End engineer and product developer.
+                <br />  
+                This is where you can find out a little bit more about what I do.
+            </p>
+            <p className="intro__text">
+                I believe that coding is a craft and true practitioners place a high premium on creating performant, scalable, standards-based code and snappy, intuitive interfaces.
+            </p>
+            <a className="intro__more-link" href="/about" title="learn more about Lewis Nixon">Read More</a>
+        </div>
+        <a className="icon-down_arrow" href="#development" data-scroll-target="#development">Scroll Down</a>
+    </section>
+    );
+  }
+}
+
+export default Intro;
