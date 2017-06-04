7358a3c1de815c4c5822e38c46e37b46007d3c1c
Lewis Nixon <lewcfn@gmail.com>
2017-06-04 18:15:05 +0100

    fix responsive styles

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import Button from '../page-link/page-link';
+import PageLink from '../page-link/page-link';
+import ScrollButton from '../scroll-button/scroll-button';
 import logo from './boxdeluxe_logo.svg';
 import './intro.css';
 
 class Intro extends Component {
   render() {
     return (
-    <section className="waypoint intro">
+    <section className="intro">
         <div className="content">
             <div className="intro__brand">
                 <img className="intro__logo" src={logo} alt="box deluxe logo" />
@@ -22,10 +23,9 @@ class Intro extends Component {
                   I believe that coding is a craft and true practitioners place a high premium on creating performant, scalable, standards-based code and snappy, intuitive interfaces.
               </p>
             </div>
-            <Button text="Read more of my profile" />
-
+            <PageLink text="Read more of my profile" />
         </div>
-        <a className="icon-down_arrow" href="#development" data-scroll-target="#development">Scroll Down</a>
+        <ScrollButton text="scroll down" scroll-to="development"/>
     </section>
     );
   }
