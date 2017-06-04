0b0fd25aa3fd36c251b0a650da5f4dd7449c5f50
Lewis Nixon <lewcfn@gmail.com>
2017-06-04 16:56:42 +0100

    style width of text content in intro

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -11,14 +11,17 @@ class Intro extends Component {
                 <img className="intro__logo" src={logo} alt="box deluxe logo" />
                 <b className="icon-star_icon intro__icon"></b>
             </div>
-            <p className="intro__text">
-                Hi, I’m Lewis Nixon, a Front End engineer and product developer.
-                <br />  
-                This is where you can find out a little bit more about what I do.
-            </p>
-            <p className="intro__text">
-                I believe that coding is a craft and true practitioners place a high premium on creating performant, scalable, standards-based code and snappy, intuitive interfaces.
-            </p>
+            <div className="intro__messaging">
+              <p className="intro__text">
+                  Hi, I’m Lewis Nixon, a Front End engineer and product developer.
+                  <br />  
+                  This is where you can find out a little bit more about what I do.
+              </p>
+              <p className="intro__text">
+                  I believe that coding is a craft and true practitioners place a high premium on creating performant, scalable, standards-based code and snappy, intuitive interfaces.
+              </p>
+            </div>
+
             <a className="intro__more-link" href="/about" title="learn more about Lewis Nixon">Read More</a>
         </div>
         <a className="icon-down_arrow" href="#development" data-scroll-target="#development">Scroll Down</a>
