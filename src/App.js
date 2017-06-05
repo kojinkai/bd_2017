fbea120d61a61bd9fc5fc9af47eaa04962fd5d58
lewisnixon <lewcfn@gmail.com>
2017-06-05 22:44:27 +0100

    feat(router): install and configure react-router-dom (#8)
    * install router and configure in App.js and masthead
    
    * remove now unused scroll button component - we're routing not scrolling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 import Masthead from './components/masthead/masthead';
 import Intro from './components/intro/intro';
 import Skills from './components/skills/skills';
 import Contact from './components/contact/contact';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="page-wrap">
-        <Masthead />
-        <Intro />
-        <Skills />
-        <Contact />
-      </div>
+      <Router>
+        <div className="page-wrap">
+          <Masthead />
+          <Route exact path="/" component={Intro}/>
+          <Route path="/skills" component={Skills}/>
+          <Route path="/contact" component={Contact}/>          
+        </div>
+      </Router>
     );
   }
 }
