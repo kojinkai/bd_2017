0c647e4f05f136f2b50a1065fded41aa939455cc
Lewis Nixon <lewcfn@gmail.com>
2017-06-06 22:22:15 +0100

    add menu toggle component and style it

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PageLink from '../page-link/page-link';
+import MenuToggle from '../menu-toggle/menu-toggle';
 import logo from './boxdeluxe_logo.svg';
 import './intro.css';
 
@@ -7,6 +8,7 @@ class Intro extends Component {
   render() {
     return (
     <section className="section intro">
+        <MenuToggle />
         <div className="content">
             <div className="intro__brand">
                 <img className="intro__logo" src={logo} alt="box deluxe logo" />
---
+++ src/components/menu-toggle/menu-toggle.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './menu-toggle.css';
+
+class MenuToggle extends Component {
+  
+  handleClick() {
+    console.log('foo menu toggle');
+  }
+
+  render() {
+    return (
+      <button className="menu-toggle" onClick={this.handleClick()}>
+        <b className="menu-toggle__icon icon-list2"></b>
+      </button>
+    );
+  }
+}
+
+export default MenuToggle;
