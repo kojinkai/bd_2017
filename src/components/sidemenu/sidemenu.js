e1f1f348a913acdfb1fca9551592c2ff1142cf04
lewisnixon <lewcfn@gmail.com>
2017-06-08 22:41:51 +0100

    feat(contact): Add and style the contact slide (#12)
    * Add a profile page and refine copy

--- src/components/sidemenu/sidemenu.js
+++ src/components/sidemenu/sidemenu.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import {
-  Link
-} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import MenuToggle from '../menu-toggle/menu-toggle';
 import './sidemenu.css';
 
