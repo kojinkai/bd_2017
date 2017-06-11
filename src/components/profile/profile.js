53c77ff3c28ccc619eb4a7f94e14a35b1889e58d
lewisnixon <lewcfn@gmail.com>
2017-06-11 15:01:29 +0100

    fix(profile): Tweak copy (#22)
    * tweak profile copy
    
    * fix typo and emphasise testability in copy

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -11,14 +11,14 @@ class Profile extends Component {
               <b className="icon-star_icon profile__icon"></b>
             </div>
             <div className="profile__messaging">
+              <p className="profile__text">I&apos;m a London based Front End developer with over 8 years experience.</p>
               <p className="profile__text">I specialise in JavaScript application and responsive UI development. I draw upon a good deal of experience working within agile product teams at the intersection of design &amp; development.</p>
-                           <p className="profile__text">Scalability and testabiity are forefront in my mind when developing.</p> 
-              <p className="profile__text">I have lots of experience using frameworks such as Angular, React and Express.js and yet I feel that good engineering practises remain paramount. Understanding the bigger pictures of scalability, modularity, patterns, anti-patterns and performance really helps you make the most of these tools.</p>                            
+              <p className="profile__text">I have lots of experience with frameworks such as Angular, React and Express and yet I feel that good engineering practises remain paramount. Understanding the bigger pictures of scalability, modularity, testability and performance really helps you make the most of these tools.</p>                            
               <p className="profile__text">Producing progressive web apps that harness new technologies to deliver great customer experiences is my main motivation and a challenge that I relish.</p>
               <p className="profile__text">I use every project as an opportunity to learn new skills and I try to operate at the sharp edge of my expertise. I keep up to date with industry news and absorb all the best-practices I can find.</p>
               <p className="profile__text">I like to adopt a test-driven approach to development. I believe that this produces better software design and helps test your understanding of the requirements before diving into a feature.</p>
-              <p className="profile__text">When done well, TDD helps with continous delivery efforts by closing feedback loops early and shortening release cycles and therefore contributes to an enjoyable, high-performance working culture.</p>
-              <p className="profile__text">I advocate integrating my work regularly and keeping batch sizes small to improve overall agility and lessen the pain of root cause analysis when things go wrong.</p>
+              <p className="profile__text">When done well, TDD helps with continous delivery efforts by closing feedback loops early and shortening release cycles and contributes to an enjoyable, high-performance working culture.</p>
+              <p className="profile__text">I advocate integrating my work with downstream changes regularly and by keeping batch sizes small I aim to improve development agility and lessen the pain of root cause analysis if things go wrong.</p>
               <p className="profile__text">I really enjoy what I do and I&apos;m motivated by working with others who do too.</p>
             </div>
         </div>
