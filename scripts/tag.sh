4c0a90c407bbb2e9d32c3ae26534a7fb83c478be
Lewis Nixon <lewcfn@gmail.com>
2017-06-14 07:14:01 +0100

    detect current branch and prune if not on master

---
+++ scripts/tag.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+latestTag=$1;
+echo latestTag $latestTag
+function cleanup {
+  echo -n "enter your tagname: "
+  read tagname
+  echo "tag set as $tagname"
+  echo -n "enter your message: "
+  echo "message set as $message"
+  read message
+
+  if [ `git rev-parse --abbrev-ref HEAD` == "master" ]; then
+    echo currently on master, rebasing and tagging...
+    git pull --rebase origin master
+    git tag -a $tagname -m $message
+    # git push origin master
+  else
+    echo "pruning ${git rev-parse --abbrev-ref HEAD}..."
+    git checkout master
+    echo currently on master, rebasing and tagging...
+    git pull --rebase origin master
+    git branch -D ${git rev-parse --abbrev-ref HEAD}
+    git tag -a $tagname -m $message
+    # git push origin master   
+  fi
+}
+
+cleanup
