889c811993a031f04920afe807980f9a3e395e7b
Lewis Nixon <lewcfn@gmail.com>
2017-06-14 20:11:41 +0100

    read the default tag from package.json

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -9,9 +9,14 @@
 # - push the tag to origin
 #
 
+# tag the code
 function tag {
-  echo -n "enter your tagname: "
+  # cat package.json and filter version number, adding a v so we get v1.1.5, say
+  local suggestedTagName=v`cat package.json | grep version | sed  "s/^.*: \"//;s/\".*//"`
+  echo -n "enter your tagname (defaults to $suggestedTagName): "
   read tagname
+  # set tag as either default or what was entered
+  tagname=${tagname:-$suggestedTagName}
   echo "tag set as $tagname"
   echo -n "enter your message: "
   read message  
@@ -20,11 +25,12 @@ function tag {
   if [ `git rev-parse --abbrev-ref HEAD` == "master" ]; then
     echo currently on master, rebasing and tagging...
     git pull --rebase origin master
-    git tag -a $tagname -m $message
-    git push origin master
+    git tag -a $tagname -m "$message"
+    git push origin $tagname
   else
     echo you are currently attempting to tag on `git rev-parse --abbrev-ref HEAD`
     echo "run cleanup.sh first to remove stale branches and re-run this script from master"
+    exit 1
   fi
 }
 
