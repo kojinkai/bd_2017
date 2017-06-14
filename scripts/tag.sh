374ade9b6388ca179891a00842a3a2bed7cfe4d1
lewisnixon <lewcfn@gmail.com>
2017-06-14 10:20:40 +0100

    chore(deploy): Add a deploy script (#26)
    * add a deploy script and tweak tag script
    
    * add a cleanup shell script and separate tagging and deployment into scripts

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
+#
+# run this on your feature / bugfix / chore branch after you have squashed
+# the branches pull request via the Github GUI.
+# - it will checkout master
+# - rebase in the upstream squashed commit
+# - delete the old, now stale branch
+# - tag the code
+# - push the tag to origin
+#
 
-latestTag=$1;
-echo latestTag $latestTag
-function cleanup {
+function tag {
   echo -n "enter your tagname: "
   read tagname
   echo "tag set as $tagname"
   echo -n "enter your message: "
+  read message  
   echo "message set as $message"
-  read message
 
   if [ `git rev-parse --abbrev-ref HEAD` == "master" ]; then
     echo currently on master, rebasing and tagging...
@@ -16,15 +23,9 @@ function cleanup {
     git tag -a $tagname -m $message
     git push origin master
   else
-    echo pruning `git rev-parse --abbrev-ref HEAD`...
-    branchToDelete=`git rev-parse --abbrev-ref HEAD`
-    git checkout master
-    echo currently on master, rebasing and tagging...
-    git pull --rebase origin master
-    git branch -D $branchToDelete
-    git tag -a $tagname -m $message
-    git push origin master
+    echo you are currently attempting to tag on `git rev-parse --abbrev-ref HEAD`
+    echo "run cleanup.sh first to remove stale branches and re-run this script from master"
   fi
 }
 
-cleanup
+tag
