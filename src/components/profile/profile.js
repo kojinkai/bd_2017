2266a868cfa129de21b7d302bad5552eb60bf499
Lewis Nixon <lewcfn@gmail.com>
2017-06-24 11:45:06 +0100

    fix(profile): Proof read copy for better grammar

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -12,14 +12,13 @@ class Profile extends Component {
             </div>
             <div className="profile__messaging">
               <p className="profile__text">I&apos;m a London based Front End developer with over 8 years experience.</p>
-              <p className="profile__text">I specialise in JavaScript application and responsive UI development. I draw upon a good deal of experience working within agile product teams at the intersection of design &amp; development.</p>
-              <p className="profile__text">I have lots of experience with frameworks such as Angular, React and Express and yet I feel that good engineering practises remain paramount. Understanding the bigger pictures of scalability, modularity, testability and performance really helps you make the most of these tools.</p>                            
+              <p className="profile__text">I specialise in JavaScript application and responsive UI development. I draw upon my experience of working cross-functionally within agile product teams.</p>
+              <p className="profile__text">I have a wealth of experience with frameworks such as Angular, React and Express which is underpinned by good engineering principles. Understanding the bigger pictures of scalability, modularity, testability and performance really helps make the most of these tools.</p>                            
               <p className="profile__text">Producing progressive web apps that harness new technologies to deliver great customer experiences is my main motivation and a challenge that I relish.</p>
               <p className="profile__text">I use every project as an opportunity to learn new skills and I try to operate at the sharp edge of my expertise. I keep up to date with industry news and absorb all the best-practices I can find.</p>
-              <p className="profile__text">I like to adopt a test-driven approach to development. I believe that this produces better software design and helps test your understanding of the requirements before diving into a feature.</p>
-              <p className="profile__text">When done well, TDD helps with continuous delivery efforts by closing feedback loops early and shortening release cycles and contributes to an enjoyable, high-performance working culture.</p>
-              <p className="profile__text">I advocate integrating my work with downstream changes regularly and by keeping batch sizes small I aim to improve development agility and lessen the pain of root cause analysis if things go wrong.</p>
-              <p className="profile__text">I really enjoy what I do and I&apos;m motivated by working with others who do too.</p>
+              <p className="profile__text">I like to adopt a test-driven approach to development. I believe that this produces better software design and helps ship features to production faster.</p>
+              <p className="profile__text">I advocate integrating my work with downstream changes regularly. Keeping batch sizes small improves development agility and lessens the pain of root cause analysis if things go wrong.</p>
+              <p className="profile__text">I really enjoy what I do and I like working with others who share a committment to producing great products and customer experiences.</p>
             </div>
         </div>
       </section>
